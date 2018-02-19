2afeb4726387ab42f9b107e2d218db5efca5b908
Chase Bernard <steven.chase.bernard@gmail.com>
2018-02-19 15:47:39 -0600

    Main Menu to Panel Functionality
    Added scripts to manage switching from the main menu to other panels in
    the UI

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour {
+
+	private GameObject activeCanvas;
+
+	public GameObject canvas0; //Main Menu
+	public GameObject canvas1; //First panel hub
+
+	public GameObject hubCanvas;
+	public GameObject panel;
+
+	private bool onMM; //Says if we are on the main menu or not
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hey activating Canvas!");
+		activeCanvas = canvas0; //First active canvas is the main menu
+		activeCanvas.SetActive(true);
+		onMM = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Canvas Change assigns different panel configurations a number, and based on
+	//that will say which one to enable and will disable the current active canvas
+	public void CanvasChange(int newCanvas)
+	{
+		activeCanvas.SetActive(false);
+
+		//Going to anywhere but the main menu
+		if (newCanvas != 0) {
+			
+			//Checks to see if we are leaving the main menu and, if so, turns on the neccessary components
+			if (onMM == true) {
+				onMM = false;
+				hubCanvas.SetActive (true);
+				panel.SetActive (true);
+			}
+			if (newCanvas == 1) {
+				activeCanvas = canvas1;
+			}
+
+			//Going to the main menu
+		} else {
+
+			//Checks to see if we are entering the main menu and, if so, turns off certain components
+			if (onMM == false) {
+				onMM = true;
+				hubCanvas.SetActive (false);
+				panel.SetActive (false);
+			}
+		}
+
+		activeCanvas.SetActive(true);
+		Debug.Log ("Hey Canvas " + newCanvas + " was activated");	
+
+
+	}
+}
---
+++ Assets/startButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startButton : MonoBehaviour {
+
+	public GameObject CanvasManager;
+
+	void Start()
+	{
+		Button btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+
+	}
+
+	void TaskOnClick()
+	{
+		CanvasManager.GetComponent<CanvasManager> ().CanvasChange (1);
+	}
+}
