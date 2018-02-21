837272ede6a7fbc1490d6f428bf76675d59060f9
Chase Bernard <steven.chase.bernard@gmail.com>
2018-02-21 15:07:31 -0600

    Add Options Menu Panel
    Added option menu panel and you can now travel back to main menu

--- Assets/CanvasManager.cs
+++ Assets/CanvasManager.cs
@@ -8,16 +8,19 @@ public class CanvasManager : MonoBehaviour {
 
 	public GameObject canvas0; //Main Menu
 	public GameObject canvas1; //First panel hub
+	public GameObject canvas4; //Options hub
 
 	public GameObject hubCanvas;
 	public GameObject panel;
 
 	private bool onMM; //Says if we are on the main menu or not
+	private Animator pAnim; //Animator for panel
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Hey activating Canvas!");
 		activeCanvas = canvas0; //First active canvas is the main menu
 		activeCanvas.SetActive(true);
+		pAnim = panel.GetComponent<Animator>();
 		onMM = true;
 	}
 	
@@ -43,16 +46,22 @@ public class CanvasManager : MonoBehaviour {
 			}
 			if (newCanvas == 1) {
 				activeCanvas = canvas1;
+				pAnim.SetInteger ("State", 1);
+			}
+			else if (newCanvas == 4) {
+				activeCanvas = canvas4;
+				pAnim.SetInteger ("State", 4);
 			}
 
 			//Going to the main menu
 		} else {
-
+			pAnim.SetInteger ("State", 0);
 			//Checks to see if we are entering the main menu and, if so, turns off certain components
 			if (onMM == false) {
 				onMM = true;
 				hubCanvas.SetActive (false);
 				panel.SetActive (false);
+				activeCanvas = canvas0;
 			}
 		}
 
---
+++ Assets/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+	public GameObject CanvasManager;
+	public int changeTo;
+
+	void Start()
+	{
+		Button btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+
+	}
+
+	void TaskOnClick()
+	{
+		CanvasManager.GetComponent<CanvasManager> ().CanvasChange (changeTo);
+	}
+}
