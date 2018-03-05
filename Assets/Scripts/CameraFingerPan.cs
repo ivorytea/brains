fd35634b94dd3c375fc0a48cfdd9f63e45e8b128
Chase Bernard <steven.chase.bernard@gmail.com>
2018-03-05 15:51:31 -0600

    Take Away Debug & Move Script
    Took out debug function and moved script to script folder

--- Assets/Scripts/CameraFingerPan.cs
+++ Assets/Scripts/CameraFingerPan.cs
@@ -8,7 +8,6 @@ public class CameraFingerPan : MonoBehaviour {
 	public float minPos = 0.07f;
 	public float maxPos = 7.07f;
 	void Update() {
-		Debug.Log (this.transform.position);
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
 				Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
 			if(transform.position.x-touchDeltaPosition.x * speed > minPos && transform.position.x-touchDeltaPosition.x * speed < maxPos)
