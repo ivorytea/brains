251aebc6305e2696c268fa5e4528099d94a8a071
Chase Bernard <steven.chase.bernard@gmail.com>
2018-03-19 15:28:24 -0500

    Continue development of ItemManager
    Created Item class in manager script, and am starting to lay out items
    based on spaces they're in and whether they are sold or not

--- Assets/ItemManager.cs
+++ Assets/ItemManager.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour {
 
+	public Sprite poster;
+	public Sprite bottles;
+	public Sprite ovalRug;
+	public Sprite starRug;
+	public Sprite squareRug;
+	public Sprite blinds;
+	public Sprite curtains;
+	public Sprite windows;
+	public Sprite dumbell;
+	public Sprite bag;
+	public Sprite jumpRope;
+	public Sprite bike;
+	public Sprite mirror;
+	public Sprite treadmill;
+
+	//Lay out all items available for each space
+	private Item[] spaceA = new Item[3];
+	private Item[] spaceB = new Item[3];
+	private Item[] spaceC = new Item[3];
+	private Item[] spaceD = new Item[3];
+	private Item[] spaceE = new Item[3];
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +35,23 @@ public class ItemManager : MonoBehaviour {
 	void Update () {
 		
 	}
+}
+
+public class Item {
+	private int cost;
+	private string name;
+	private bool sold;
+	private Sprite sprite;
+
+	public Item(int c, string n, Sprite s){
+		cost = c;
+		name = n;
+		sold = false;
+		sprite = s;
+	}
+
+	public void sellItem(){
+		sold = true;
+	}
+
 }
