a13d73c4c2df69ec600d9efbc8f28589e046a6bc
Bryan Stamey <bryanstamey@Bryans-Mac-Pro.attlocal.net>
2018-04-15 21:19:21 -0500

    added base code to alter

---
+++ Assets/Scripts/Matching.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matching : MonoBehaviour {
+
+	public Image[] shapeColor;
+	public Button[] shapeButtons;
+	public Button wrongButton;
+	public Image wrongShape;
+	public Image shapeFace;
+
+	public Shapes_CanvasManager canvasManager;
+
+	void Start() {
+
+		// Hide all colored images initially
+		for (int i = 0; i < shapeColor.Length; i++) {
+			shapeColor [i].enabled = false;
+		}
+
+		// Hide final shape and X initially
+		wrongShape.enabled = false;
+		shapeFace.enabled = false;
+
+		for (int i = 0; i < shapeButtons.Length; i++) {
+			int closureIndex = i;
+			shapeButtons[i] = shapeButtons[i].GetComponent<Button> ();
+			shapeButtons[closureIndex].onClick.AddListener(() => btnClick(closureIndex));
+		}
+
+		// Wrong button click
+		wrongButton = wrongButton.GetComponent<Button> ();
+		wrongButton.onClick.AddListener (() => wrongClick ());
+	}
+
+	void Update() {
+		if (isWin ()) {
+			win ();
+			StartCoroutine (pauseWin());
+		}
+	}
+
+	public void btnClick(int buttonNum) {
+		Debug.Log ("buttonNum: " + buttonNum);
+		shapeColor [buttonNum].enabled = true;
+		shapeButtons[buttonNum].gameObject.SetActive(false);
+	}
+
+	public void wrongClick () {
+		Debug.Log ("wrongClick");
+		wrongShape.enabled = true;
+		// Reset shape
+		for (int i = 0; i < shapeColor.Length; i++)
+			shapeColor [i].enabled = false;
+		// Reset buttons
+		for (int i = 0; i < shapeButtons.Length; i++)
+			shapeButtons [i].gameObject.SetActive (true);
+		StartCoroutine (pauseWrong ());
+	}
+
+	public void resetX() {
+		wrongShape.enabled = false;
+	}
+
+	public bool isWin() {
+		bool win = false;
+		for (int i = 0; i < shapeColor.Length; i++) {
+			if (shapeColor [i].enabled == false)
+				return false;
+			else
+				win = true;
+		}
+		return win;
+	}
+
+	public void win () {
+		Debug.Log ("Win!");
+		shapeFace.enabled = true;
+	}
+
+	IEnumerator pauseWin() {
+		yield return new WaitForSeconds (2);
+		canvasManager.winDetected (true);
+	}
+
+	// TODO: Ditto
+	IEnumerator pauseWrong() {
+		yield return new WaitForSeconds (2);
+		Debug.Log ("Delaying...");
+		resetX ();
+	}
+}
---
+++ Assets/Scripts/Matching_CanvasManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matching_CanvasManager : MonoBehaviour {
+
+	public GameObject[] canvas;
+	public GameObject scoreCanvas;
+	System.Random rnd = new System.Random();
+	public bool[] haveSeen;
+	private int score = 0;
+	public Text scoreText;
+	private float timeLeft = 30.0f; // Set to game time in seconds
+	public Text timeDisp;
+	private bool gameOver = false;
+
+	void Start() {
+		scoreCanvas.SetActive (false);
+		//scoreText = GetComponent<Text> ();
+		switchCanvas ();
+	}
+
+	void Update() {
+		timeLeft -= Time.deltaTime;
+		timeDisp.text = timeLeft.ToString ();
+		if ( timeLeft < 0 )
+		{
+			gameOver = true;
+		}
+
+		if (scoreText == null) {
+			Debug.Log ("ERR: scoreText set to null");
+			Debug.Break ();
+		}
+
+		// TODO: refactor
+		if (gameOver) {
+			Debug.Log ("Score is " + score);
+			for (int i = 0; i < canvas.Length; i++)
+				canvas [i].SetActive (false);
+			scoreCanvas.SetActive (true);
+			scoreText.text = score.ToString ();
+		}
+	}
+
+	void switchCanvas() {
+
+		if (allPlayed ()) {
+			// Turn off shapes and move to score screen
+			Debug.Log ("Played all shapes");
+			Debug.Log ("Score is " + score);
+			for(int i = 0; i < canvas.Length; i++)
+				canvas [i].SetActive (false);
+			// Compute score
+			scoreCanvas.SetActive (true);
+			scoreText.text = score.ToString ();
+		}
+
+		else {
+			// generate new canvas
+			int idx = generateRandomCanvas ();
+			// find shape not yet played
+			while (played(idx))
+				idx = generateRandomCanvas ();
+			// turn on canvas
+			canvas[idx].SetActive (true);
+			// turn off other canvases
+			for (int i = 0; i < canvas.Length; i++) {
+				if (i != idx) 
+					canvas [i].SetActive (false);
+			}
+			haveSeen [idx] = true;
+		}
+	}
+
+	// Called from Shape.cs if shape is won
+	public void winDetected(bool win) {
+		score += 10;  // Just adding 10 to score on a win for testing
+		switchCanvas();
+	}
+
+	// Determines if a shape has been played
+	// Returns: true if shape has been played, 
+	//          false if not yet played
+	bool played(int idx) {
+		if (haveSeen [idx])
+			return true;
+		return false;
+	}
+
+	// Generates the index of a random canvas
+	// Returns: randomly generated index
+	int generateRandomCanvas() {
+		int idx = rnd.Next (1, canvas.Length);
+		return idx;
+	}
+
+	// Determines if all shapes have been played
+	// Returns: false if outstanding shapes remain, 
+	//          true if all are played
+	bool allPlayed() {
+		for(int i = 0; i < canvas.Length; i++) {
+			if (!haveSeen[i])
+				return false;
+		}
+		return true;
+	}
+}
